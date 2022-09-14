68d3e3c29c0edc509ebc61403d22a0782913c323
Fikri Bill Gufran <muhammad.fikri@sirclo.com>
2022-09-14 14:16:30 +0800

    update metatag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import "styles/globals.css";
+import Head from "next/head";
 import type { AppProps } from "next/app";
+import "styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Colors Challenge</title>
+        <meta name="description" content="Submission for Brikl Frontend - React, TypeScript position" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
