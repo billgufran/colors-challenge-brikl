2407b6c05d404d706bba4d43c93e24a30ec5fe6d
Fikri Bill Gufran <muhammad.fikri@sirclo.com>
2022-09-14 21:01:41 +0800

    error state

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,52 +5,64 @@ import { useEffect, useState } from "react";
 import styles from "styles/pages/Home.module.css";
 import { TColor } from "types";
 
+const fetchColors = async () => {
+  const response = await fetch("/api/colors", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`Error: ${response.status}`);
+  }
+
+  const data = await response.json();
+
+  return data;
+};
+
 const Home: NextPage = () => {
   const [colors, setColors] = useState<TColor[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
-  const fetchColors = async () => {
+  const getData = async () => {
     setIsLoading(true);
-    const response = await fetch("/api/colors", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (!response.ok) {
+    setError("");
+    try {
+      const data = await fetchColors();
+      setColors(data);
+    } catch (error) {
+      console.error(error);
+      setError(error instanceof Error ? error.message : "An error occurred");
+    } finally {
       setIsLoading(false);
-      throw new Error(`Error: ${response.status}`);
     }
-
-    const data = await response.json();
-    setIsLoading(false);
-    setColors(data);
   };
 
   useEffect(() => {
-    try {
-      fetchColors();
-    } catch (error) {
-      console.error(error);
-    }
+    getData();
   }, []);
 
   return (
     <div className={styles.container}>
+      {error && <h3 className={styles.error}>{error}</h3>}
       {colors.length > 0 &&
         colors.map((color, index) => <ColorBar key={index} {...color} />)}
       <div>
         <LinearProgressBar load={isLoading} width={isLoading ? "9px" : "2px"} />
         <div className={styles.footer}>
           <p>brikl &bull; Colors Challenge</p>
-          <button
-            onClick={fetchColors}
-            className={styles.generateButton}
-            disabled={isLoading}
-          >
-            Generate
-          </button>
+          {(colors.length > 0 || error) && (
+            <button
+              onClick={getData}
+              className={styles.generateButton}
+              disabled={isLoading}
+            >
+              Generate
+            </button>
+          )}
         </div>
       </div>
     </div>
